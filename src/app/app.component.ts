d500b8eab6a472148a613eb2f1ab8552ddf181ed
niharika.gupta <niharika.gupta@renewbuy.com>
2022-11-13 15:55:24 +0530

    initial table commitment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import dataJson from './datasource.json';
+
+export interface UserData {
+    machineNumber: string;
+    serialNumber: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'clavax-data-table';
+    title = 'clavax-data-table';
+
+    displayedColumns: string[] = ['machineNumber', 'serialNumber'];
+    dataSource: any = new MatTableDataSource();
+
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+    constructor() {
+        // Assign the data to the data source for the table to render
+        let machineData: any = [];
+        dataJson.forEach(ele => {
+            machineData.push(ele['content']['machineInformation']);
+        });
+
+        this.dataSource.data = machineData;
+    }
+    isGroup(index:any, item:any): boolean {
+        return item.level;
+      }
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatTableModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatPaginatorModule,
+        MatIconModule,
+        MatMenuModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
